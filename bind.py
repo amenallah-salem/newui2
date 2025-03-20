c13893a334cc49491be2130553abda37ba1685e9
Amenallah Salem <salem.amenalah@gmail.com>
2025-03-20 14:24:15 +0000

    update

--- bind.py
+++ bind.py
@@ -7,7 +7,7 @@ def run():
     width = st.sidebar.slider("Set Width", min_value=400, max_value=1200, value=800)
     height = st.sidebar.slider("Set Height", min_value=300, max_value=800, value=600)
 
-    st.components.v1.iframe("http://localhost:5173/", width=width, height=height, scrolling=True)
+    st.components.v1.iframe("http://0.0.0.0:8080", width=width, height=height, scrolling=True)
 
 if __name__ == "__main__":
     run()
