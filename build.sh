588e0ffd9fb971fc5c89bb96a491c70249bc2d67
Amenallah Salem <salem.amenalah@gmail.com>
2025-03-24 12:59:14 +0000

    update build sh

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ docker compose -f docker-compose-build-pycli.yml up --build
 
 #docker ps 
 #docker run -it CONT_ID 
-#docker cp CONT_ID:/app/releases/* ./releases
+#sudo docker cp CONT_ID:/app/releases/ ./releases
+#zip release.zip ./releases
